e29252352a22f294de4f332abc1d0b3019784341
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-05-17 21:54:12 +0100

    Sequelize integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { sequelizeConfig } from './config/sequelize.config';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import databaseConfig from './config/database.config';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [databaseConfig],
-    }),
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot(sequelizeConfig),
+    UsersModule,
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
