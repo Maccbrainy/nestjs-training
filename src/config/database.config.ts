d52106ae347d9bd6a25e5a58fe11065eceb5238f
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-05-17 18:21:55 +0100

    file removed

--- src/config/database.config.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import testConfig from './test.config';
-import developmentConfig from './development.config';
-
-const configuration =
-  process.env.NODE_ENV === 'test' ? testConfig : developmentConfig;
-export default () => configuration;
